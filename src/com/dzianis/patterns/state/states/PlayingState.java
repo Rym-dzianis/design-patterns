6186ec05c24f25c0cd59d50457116a9518e9c22e
Dzianis <rym-dzianis@mail.ru>
2018-04-17 14:58:05 +0300

    state

---
+++ src/com/dzianis/patterns/state/states/PlayingState.java
@@ -0,0 +1,33 @@
+package com.dzianis.patterns.state.states;
+
+import com.dzianis.patterns.state.ui.Player;
+
+public class PlayingState extends State {
+
+    public PlayingState(Player player) {
+        super(player);
+    }
+
+    @Override
+    public String onLock() {
+        player.changeState(new LockedState(player));
+        player.setCurrentTrackAfterStop();
+        return "Stop playing";
+    }
+
+    @Override
+    public String onPlay() {
+        player.changeState(new ReadyState(player));
+        return "Paused...";
+    }
+
+    @Override
+    public String onNext() {
+        return player.nextTrack();
+    }
+
+    @Override
+    public String onPrevious() {
+        return player.previousTrack();
+    }
+}
